020789e532f19a8e2df913b27bd152ed106232f9
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-02 00:21:31 +0100

    Update test_HctCorrection.m
    function name updated for consistency; comments revised for clarity

--- test/test_HctCorrection.m
+++ test/test_HctCorrection.m
@@ -1,42 +1,43 @@
-function test_HctCorrection()
+function test_hctCorrection()
+%% TEST_HCTCORRECTION Unit tests for the hctCorrection function.
 %
-% @ Author: Dr. Tanuj Puri
-% @ Date:   01/2014
-% @ Warning: This is an untested code/implementation and should be used
+% Author: Dr. Tanuj Puri
+% Date:   01/2014
+% Warning: This is an untested code/implementation and should be used
 % with caution in clinical and pre-clinical settings. The author takes no 
 % responsibility of any kind about the output results from this code.
 %
-    % Define all test cases as strings
+%% Define all test cases as strings
     tests = {
-        "Cp = HctCorrection([1.5, 2.0], 0.38);"     % Custom Hct
-        "Cp = HctCorrection(5.2);"                  % Default Hct
-        "Cp = HctCorrection([2.0; 3.0], 0.35);"     % Custom Hct, column vector
-        "Cp = HctCorrection(5);"                    % Scalar input
-        "Cp = HctCorrection([1.0, 2.5, 3.0]);"      % Row vector
-        "Cp = HctCorrection([1.0; 2.0; 3.0], 0.30);" % Column vector
-        "Cp = HctCorrection(0);"                    % Zero input
-        "Cp = HctCorrection([0, 1.1, 2.2], 0.25);"   % Mixed zero & positive
-        "Cp = HctCorrection([1 2; 3 4]);"            % Matrix input
-        "Cp = HctCorrection([-1, 2, 3]);"            % Negative input
-        "Cp = HctCorrection([1, NaN]);"             % NaN
-        "Cp = HctCorrection([1, Inf]);"             % Inf
-        "Cp = HctCorrection([1 + 2i]);"             % Complex
-        "Cp = HctCorrection('string');"             % Non-numeric
-        "Cp = HctCorrection(1.2, -0.1);"            % Hct < 0
-        "Cp = HctCorrection(1.2, 1);"               % Hct = 1
-        "Cp = HctCorrection(1.2, 1.5);"             % Hct > 1
-        "Cp = HctCorrection(1.2, NaN);"             % Hct NaN
-        "Cp = HctCorrection(1.2, Inf);"             % Hct Inf
-        "Cp = HctCorrection(1.2, [0.3 0.4]);"        % Hct vector
-        "Cp = HctCorrection(1.2, 'high');"          % Hct non-numeric
-        "Cp = HctCorrection();"                    % Too few args
-        "Cp = HctCorrection(1.2, 0.4, 999);"        % Too many args
-        "Cp = HctCorrection([1.0, 2.5, 3.0],[0.3, 0.2]);" % Hct not scaler
-        "[a, b] = HctCorrection(1.2);"              % Too many outputs
+        "Cp = hctCorrection([1.5, 2.0], 0.38);"     % Custom Hct
+        "Cp = hctCorrection(5.2);"                  % Default Hct
+        "Cp = hctCorrection([2.0; 3.0], 0.35);"     % Custom Hct, column vector
+        "Cp = hctCorrection(5);"                    % Scalar input
+        "Cp = hctCorrection([1.0, 2.5, 3.0]);"      % Row vector
+        "Cp = hctCorrection([1.0; 2.0; 3.0], 0.30);" % Column vector
+        "Cp = hctCorrection(0);"                    % Zero input
+        "Cp = hctCorrection([0, 1.1, 2.2], 0.25);"   % Mixed zero & positive
+        "Cp = hctCorrection([1 2; 3 4]);"            % Matrix input
+        "Cp = hctCorrection([-1, 2, 3]);"            % Negative input
+        "Cp = hctCorrection([1, NaN]);"             % NaN
+        "Cp = hctCorrection([1, Inf]);"             % Inf
+        "Cp = hctCorrection([1 + 2i]);"             % Complex
+        "Cp = hctCorrection('string');"             % Non-numeric
+        "Cp = hctCorrection(1.2, -0.1);"            % Hct < 0
+        "Cp = hctCorrection(1.2, 1);"               % Hct = 1
+        "Cp = hctCorrection(1.2, 1.5);"             % Hct > 1
+        "Cp = hctCorrection(1.2, NaN);"             % Hct NaN
+        "Cp = hctCorrection(1.2, Inf);"             % Hct Inf
+        "Cp = hctCorrection(1.2, [0.3 0.4]);"        % Hct vector
+        "Cp = hctCorrection(1.2, 'high');"          % Hct non-numeric
+        "Cp = hctCorrection();"                    % Too few args
+        "Cp = hctCorrection(1.2, 0.4, 999);"        % Too many args
+        "Cp = hctCorrection([1.0, 2.5, 3.0],[0.3, 0.2]);" % Hct not scaler
+        "[a, b] = hctCorrection(1.2);"              % Too many outputs
     };
 
     % Track results
-    fprintf('--- Running HctCorrection Test Suite ---\n\n');
+    fprintf('--- Running hctCorrection Test Suite ---\n\n');
     for i = 1:length(tests)
         testLine = tests{i};
         try
