e574e909570e8264758ff8f4aef0a410beb20b34
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-05 16:29:57 +0100

    Update load_Ct.m

--- load_Ct.m
+++ load_Ct.m
@@ -1,7 +1,7 @@
 function [Ct] = load_Ct(varargin)
 %% LOAD_CT Load a Ct (tissue time-activity curve) vector from a .mat file and perform strict validation.
 % 
-%   Ct = load_Ct() loads the default file 'D:\Ct_D_0.1.mat'.
+%   Ct = load_Ct() loads the default file 'Ct.mat' in the current directory.
 %   Ct = load_Ct(filepath) loads the Ct vector from the specified file.
 %
 %   The loaded data must:
