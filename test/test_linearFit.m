5886a36058e2e3101210d9b9328effdd99105fed
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-06 01:57:18 +0100

    Update test_linearFit.m

--- test/test_linearFit.m
+++ test/test_linearFit.m
@@ -1,4 +1,5 @@
 function test_linearFit()
+%
 % TEST_LINEARFIT Unit tests for the linearFit function.
 %
 % This test suite checks:
@@ -8,7 +9,7 @@ function test_linearFit()
 %   - R-squared values in expected ranges
 %
 % @ Author: Dr. Tanuj Puri
-% @ Date:   01/2014
+% @ Date:   01/2014, updated 2025
 % @ Warning: This is an untested code/implementation and should be used
 % with caution in clinical and pre-clinical settings. The author takes no 
 % responsibility of any kind about the output results from this code.
