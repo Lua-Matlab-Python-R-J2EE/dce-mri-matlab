50ceeca1f6004b91d3b73b2fea05a6fadc49f72f
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-06 01:46:34 +0100

    Update hctCorrection.m

--- hctCorrection.m
+++ hctCorrection.m
@@ -3,7 +3,7 @@ function [Cp] = hctCorrection(Cb, Hct)
 % from whole blood concentration (Cb), corrected for hematocrit (Hct)
 %
 % Author: Dr. Tanuj Puri
-% Date:   01/2014
+% Date:   01/2014, updated 2025
 % Warning: This is an untested code/implementation and should be used
 % with caution in clinical and pre-clinical settings. The author takes no 
 % responsibility of any kind about the output results from this code.
