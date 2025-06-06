6a40836f677f253fe5c9d50cc431abc0519767d2
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-06 01:45:09 +0100

    Update create_Realistic_Ct.m

--- create_Realistic_Ct.m
+++ create_Realistic_Ct.m
@@ -8,7 +8,7 @@ function Ct = create_Realistic_Ct()
 % It also saves the Ct values as a .mat file.
 %
 % Author: Dr. Tanuj Puri 
-% Date: 01/2014 
+% Date:   01/2014, updated 2025
 % Warning: This is an untested code/implementation and should be used
 % with caution in clinical and pre-clinical settings.
 %
