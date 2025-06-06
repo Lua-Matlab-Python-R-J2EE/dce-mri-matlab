3c15e526d7e6a48d264e8c785f0dfbf57e0261ab
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-06 01:55:19 +0100

    Update test_getInfMap.m

--- test/test_getInfMap.m
+++ test/test_getInfMap.m
@@ -5,7 +5,7 @@ function test_getInfMap()
 % input validation, 2D and 3D structures, and visualization flag behavior.
 %
 % Author: Dr. Tanuj Puri 
-% Date: 01/2014
+% Date: 01/2014, updated 2025
 % Warning: This is an untested code/implementation and should be used
 % with caution in clinical and pre-clinical settings.
 %
