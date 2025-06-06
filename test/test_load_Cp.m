5e7cf6a3f99c9c5e7bdc6539328bcf319dc263e6
Scientific Programmer <mathstutorlondonuk@gmail.com>
2025-06-06 01:57:35 +0100

    Update test_load_Cp.m

--- test/test_load_Cp.m
+++ test/test_load_Cp.m
@@ -4,7 +4,7 @@ function test_load_Cp()
 % Tests valid Cp loading, edge cases, and validation error handling.
 %
 % Author: Dr. Tanuj Puri
-% Date:   01/2014
+% Date:   01/2014. updated 2025
 % Warning: This is an untested code/implementation and should be used
 % with caution in clinical and pre-clinical settings.
 %
